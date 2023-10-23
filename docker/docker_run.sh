cf1df10a716bb2c0fc2206cf212bbe70e7a9ee48
YoshikiMaekawa2000 <ce232091@meiji.ac.jp>
2023-10-23 11:14:31 +0900

    update

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,6 +1,8 @@
 image_name='airsim_source'
 tag_name="unreal_engine_4.27"
 
+xhost +local:root
+
 docker run \
     --ipc=host \
     -it \
@@ -8,5 +10,8 @@ docker run \
     --rm \
     --gpus all \
     --name "airsim_source" \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     $image_name:$tag_name \
     bash
+
